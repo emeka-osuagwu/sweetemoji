49dffc52faa505fb8bef854c88e1883432173ddc
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2015-11-04 13:05:55 +0100

    Remove create.php

--- tests/TestEntity.php
+++ tests/TestEntity.php
@@ -4,20 +4,12 @@ namespace Test;
 
 use PDO;
 use Mockery;
-use PHPUnit_Framework_TestCase;
-use Emeka\Candy\Model\GetEntity;
-use Emeka\Candy\Model\FindEntity;
-use Emeka\Candy\Model\SaveEntity;
-use Emeka\Candy\Model\WhereEntity;
-use Emeka\Candy\Model\DeleteEntity;
 
 class TestEntity extends PHPUnit_Framework_TestCase
 {
 	
 	public function testGetEntity()
 	{	    
-	    
-
 	    $this->assertJson(GetEntity::all('users', $dbConnMocked));	      
 	}
 }
